c7f48fbeaa888b9662b3578c76407d16803da83c
Zack Williamson <zwilliamson@outlook.com>
2022-08-02 09:49:08 -0400

    API integration, cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { Request, Response } from "express";
 import { Firebot } from "firebot-custom-scripts-types";
 const localtunnel = require("localtunnel");
 
 interface Params {
   rootUrl: string;
 }
+
+let tunnel: any;
+let tunnelRootUrl: string;
 
 const script: Firebot.CustomScript<Params> = {
   getScriptManifest: () => {
@@ -19,20 +23,78 @@ const script: Firebot.CustomScript<Params> = {
     return {
       rootUrl: {
         type: "string",
-        default: "https://localtunnel.me",
+        default: "",
         description: "Root URL",
-        secondaryDescription: "Enter the root URL for the localtunnel instance you wish to use (or the default of https://localtunnel.me)",
+        secondaryDescription: "Enter the root URL for the localtunnel instance you wish to use, or leave blank to use the default of https://localtunnel.me",
       },
     };
   },
   run: async ({ parameters, modules, firebot }) => {
-    const { logger } = modules;
-    logger.info(`Connecting to ${parameters.rootUrl} to create tunnel to http://locahost:${firebot.settings.getWebServerPort()}`);
+    const { logger, httpServer } = modules;
+    const port = firebot.settings.getWebServerPort();
+    const rootUrl = parameters.rootUrl || "https://localtunnel.me";
 
-    const tunnel = await localtunnel({ host: parameters.rootUrl, port: firebot.settings.getWebServerPort() });
+    logger.info(`[localtunnel] Connecting to ${rootUrl} to create tunnel to http://locahost:${port}...`);
 
-    logger.info(`Tunnel URL ${tunnel.url} connected to http://locahost:${firebot.settings.getWebServerPort()}`);
-  },
+    tunnel = await localtunnel({ host: rootUrl, port: port });
+    tunnelRootUrl = tunnel.url.replace("http:", "https:");
+
+    logger.info(`[localtunnel] Tunnel URL ${tunnelRootUrl} connected to http://locahost:${port}`);
+
+    httpServer.unregisterCustomRoute("localtunnel", "status", "GET");
+    httpServer.unregisterCustomRoute("localtunnel", "tunnel", "GET");
+    httpServer.unregisterCustomRoute("localtunnel", "tunnel", "POST");
+    httpServer.unregisterCustomRoute("localtunnel", "tunnel", "DELETE");
+
+    httpServer.registerCustomRoute("localtunnel", "status", "GET", async function (req: Request, res: Response) {
+      res.send({
+        status: "localtunnel Plugin is running",
+        tunnelConnected: !tunnel.closed,
+        tunnelRootUrl: tunnel.closed !== true ? tunnelRootUrl : null
+      });
+    });
+
+    httpServer.registerCustomRoute("localtunnel", "tunnel", "GET", async function (req: Request, res: Response) {
+      res.redirect(`${tunnelRootUrl}/integrations/localtunnel/status`);
+    });
+
+    httpServer.registerCustomRoute("localtunnel", "tunnel", "POST", async function (req: Request, res: Response) {
+      try {
+        await new Promise((resolve, reject) => tunnel.open((err: string) => (err ? reject(err) : resolve(tunnel))));
+        tunnelRootUrl = tunnel.url.replace("http:", "https:");
+      } catch (error) {
+        res.statusCode = 500;
+        res.send({
+          result: "Failed to open tunnel",
+          error: error
+        });
+      }
+
+      res.send({
+        result: "Tunnel opened",
+        tunnelConnected: !tunnel.closed,
+        tunnelRootUrl: tunnel.closed !== true ? tunnelRootUrl : null
+      });
+    });
+
+    httpServer.registerCustomRoute("localtunnel", "tunnel", "DELETE", async function (req: Request, res: Response) {
+      try {
+        tunnel.close();
+      } catch (error) {
+        res.statusCode = 500;
+        res.send({
+          result: "Failed to close tunnel",
+          error: error
+        });
+      }
+
+      res.send({
+        result: "Tunnel closed",
+        tunnelConnected: !tunnel.closed,
+        tunnelRootUrl: tunnel.closed !== true ? tunnelRootUrl : null
+      });
+    });
+  }
 };
 
 export default script;
